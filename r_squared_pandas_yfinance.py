f594a9f5d2d5dc5079405ab69559b2f6586c2435
joshuabecker91 <joshua.becker91@gmail.com>
2022-12-16 12:55:24 -0800

    update with r squared and backtesting

--- r_squared_pandas_yfinance.py
+++ r_squared_pandas_yfinance.py
@@ -1,16 +1,21 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+import numpy
+import yfinance as yf
+import statistics
+# import csv
 # pip install pandas
 # pip install numpy
+# pip install yfinance
+
+# other method for r squared
+# from sklearn.linear_model import LinearRegression
 # pip install scipy
 # pip install scikit-learn
-import yfinance as yf
-# pip install yfinance
-import pendulum
-import matplotlib.pyplot as plt
+
+# for charts
+# import pendulum
+# import matplotlib.pyplot as plt
 # pip install matplotlib pendulum
-# import csv
-import statistics
 
 # ---------------------------------------------------------------------------------------
 
@@ -120,10 +125,9 @@ print(df)
 df.to_csv('result.csv')
 
 
+# Standard Deviation of Spread
 st_dev = statistics.stdev(spread)
 print(st_dev)
-
-# Standard Deviation of Spread
 
 
 # ---------------------------------------------------------------------------------------
@@ -173,9 +177,22 @@ total_capital = capital_leg_1 + capital_leg_2
 print(total_capital)
 total_return = (sum(trades_pnl)*100) / total_capital
 
-print("average ratio", average_ratio)
-print("total return", total_return*100)
 
+print("standard dev: ", st_dev)
+print("average ratio", average_ratio)
+print("total return", total_return*100, "%")
+
+
+# we want to make it monitor the ratios every minute, not just once a day...
+
+
+# r squared -----------------------------------------------------------------------------
+
+corr_matrix = numpy.corrcoef(time_series1, time_series2)
+corr = corr_matrix[0,1]
+R_sq = corr**2
+
+print("r squared: ", R_sq)
 
 # ---------------------------------------------------------------------------------------
 
@@ -183,48 +200,4 @@ print("total return", total_return*100)
 # dt_list = [pendulum.parse(str(dt)).float_timestamp for dt in list(price_history.index)]
 # plt.style.use('dark_background')
 # plt.plot(dt_list, time_series, linewidth=2)
-
-# Notes for Standard Dev ----------------------------------------------------------------
-
-#create DataFrame
-# df = pd.DataFrame({'hours': [1, 2, 2, 4, 2, 1, 5, 4, 2, 4, 4, 3, 6],
-#                    'prep_exams': [1, 3, 3, 5, 2, 2, 1, 1, 0, 3, 4, 3, 2],
-#                    'score': [76, 78, 85, 88, 72, 69, 94, 94, 88, 92, 90, 75, 96]})
-
-#view DataFrame
-# print(df)
-
-#     hours  prep_exams  score
-# 0       1           1     76
-# 1       2           3     78
-# 2       2           3     85
-# 3       4           5     88
-# 4       2           2     72
-# 5       1           2     69
-# 6       5           1     94
-# 7       4           1     94
-# 8       2           0     88
-# 9       4           3     92
-# 10      4           4     90
-# 11      3           3     75
-# 12      6           2     96
-
-#initiate linear regression model
-# model = LinearRegression()
-
-#define predictor and response variables
-# X, y = df[[time_series1, time_series2]], df.score
-
-#fit regression model
-# model.fit(X, y)
-
-#calculate R-squared of regression model
-# r_squared = model.score(X, y)
-
-#view R-squared value
-# print(r_squared)
-
-# 0.7175541714105901
-
-
 
